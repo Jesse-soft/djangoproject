89dd068d5a790550c904dc18b9882ffa882244a7
Jesse-soft <onyedirejesse@gmail.com>
2024-10-14 11:09:14 -0700

    new commit

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 # Create your models here.
 
-class TodoItem(models.Model):
-    title = models.CharField(max_length=200)
-    completed = models.BooleanField(default=False)
+class Room(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True,blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+def __str__ (self):
+    return self.name
