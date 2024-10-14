ac0c597404ecb2092222b116b1e4fc0723e482b7
Jesse-soft <onyedirejesse@gmail.com>
2024-10-13 17:43:32 -0700

    new commit

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("",views.home, name="home"),
+    path("room_page/<int:pk>/",views.room, name="room"),
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+rooms=[
+    {'id':1,'name':'lets learn python'},
+    {'id':2, 'name':'design with me'},
+    {'id':3, 'name':'frontend developers'}, 
+       ]
+
 def home(request):
-    return render(request, "home.html")
+    context = {'rooms':rooms}
+    return render(request, "home.html",context)
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id']== pk:
+            room=i
+    context = {'room':room}
+    return render(request, "room.html",context)
